4f52c1ad496814630d5cba54b2cbb39dcb120be3
Miika Härmä <harmamiika@gmail.com>
2021-07-15 23:37:38 +0300

    first 3 exercises done

---
+++ healthcalculators/bmiCalculator.ts
@@ -0,0 +1,26 @@
+const calculateBmi = (heightInCm: number, weightInKg: number): string => {
+  const bmi = weightInKg / (heightInCm / 100) ** 2;
+
+  let weightClass: string;
+  if (bmi < 18.5) {
+    weightClass = 'Underweight';
+  } else if (bmi >= 18.5 && bmi < 25) {
+    weightClass = 'Normal (healthy weight)';
+  } else if (bmi >= 25 && bmi < 30) {
+    weightClass = 'Overweight';
+  } else {
+    weightClass = 'Obese';
+  }
+
+  return weightClass;
+};
+
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+
+if (height && weight) {
+  const result = calculateBmi(height, weight);
+  console.log(result);
+} else {
+  console.log('invalid arguments');
+}
---
+++ healthcalculators/exerciseCalculator.ts
@@ -0,0 +1,75 @@
+interface exerciseCalcResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const target = parseInt(process.argv[2]);
+
+const exerciseHoursList = process.argv
+  .slice(3, process.argv.length - 1)
+  .map((c) => parseInt(c));
+
+console.log(target, 'target');
+console.log(exerciseHoursList, 'list');
+
+if (process.argv.length < 3) {
+  console.log('Not enough arguments from the command line!');
+}
+
+const calculateExercises = (
+  exerciseHoursList: number[],
+  target: number
+): exerciseCalcResult | null => {
+  if (!exerciseHoursList || !target) {
+    return null;
+  }
+
+  const periodLength = exerciseHoursList.length;
+
+  const trainingDays = exerciseHoursList.reduce((total, currentValue) => {
+    if (currentValue > 0) {
+      return total + 1;
+    } else {
+      return total;
+    }
+  });
+
+  const success = trainingDays >= target ? true : false;
+
+  let rating: number;
+  let ratingDescription: string;
+  if (trainingDays >= target) {
+    rating = 3;
+    ratingDescription = 'Good job!';
+  } else if (trainingDays >= target / 2) {
+    rating = 2;
+    ratingDescription = 'Not bad but could be better!';
+  } else {
+    rating = 1;
+    ratingDescription = 'Cmon man...';
+  }
+
+  const average =
+    exerciseHoursList.reduce((total, current) => total + current) /
+    exerciseHoursList.length;
+
+  const summaryObject = {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+
+  return summaryObject;
+};
+
+const result = calculateExercises(exerciseHoursList, target);
+console.log(result);
